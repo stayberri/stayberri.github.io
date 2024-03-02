5a1559d7e6e915fe54ad37065bbf65adca5cebd0
Desii S <diallusions@gmail.com>
2024-03-02 14:12:12 -0500

    Forgot how GitHub Pages worked. I'll keep this as a proof of concept I suppose

---
+++ pages/config.js
@@ -0,0 +1,4 @@
+// pretend this file is hidden. I wont actually have a proper dev system unless i privately host. I guess this is more for proof of concept
+var config = {
+    devPassword: "this_is_a_test_password"
+}
